f7d4c53c8414438a8007fe1f4cce07ade1186968
zachpuck <zachpuckett@gmail.com>
2017-12-31 14:41:10 -0800

    refactor to copy directory inside of container

--- cmd.js
+++ cmd.js
@@ -2,6 +2,49 @@ const variables = require('./variables.json');
 const fs = require('fs');
 const path = require('path');
 
+// copy templates into result before processing
+const copyFileSync = (source, target) => {
+    let targetFile = target;
+    
+    //if target is a directory a new file with the same name will be created
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+};
+
+const copyFolderRecursiveSync = (source, target) => {
+    let files = [];
+
+    // check if path.basename is source root directory
+    let sourcePathBasename = '';
+    if (path.basename(source) !== 'templates') {
+        sourcePathBasename = path.basename(source);
+    }
+
+    //check if folder needs to be created or integrated
+    let targetFolder = path.join(target, sourcePathBasename);
+
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
+    }
+
+    //copy
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach( file => {
+            let curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        } );
+    }
+};
+
 const processDirs = (dirPath) => {
     fs.readdirSync(dirPath).forEach(item => {
         const itemAbsPath = `${dirPath}/${item}`;
@@ -30,5 +73,5 @@ const fileEnvsubst = (itemAbsPath, file) => {
     fs.writeFileSync(itemAbsPath, hydratedFile);
 };
 
+copyFolderRecursiveSync('/templates', '/result');
 processDirs('/result');
-
